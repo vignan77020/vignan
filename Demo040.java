0ab38dcadcbb04ff29add3c1e7ac04fdad6361a8
vignan77020 <vignanreddynani@gmail.com>
2025-01-26 15:35:53 +0530

    to check lowercase or uppercase or digit or special

---
+++ Demo040.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Demo040 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Character: ");
+        char character = sc.next().charAt(0);
+        if (Character.isLowerCase(character)){
+            System.out.println("Entered character is in lower case");
+        }else if (Character.isUpperCase(character)){
+            System.out.println("Entered character is in Upper case");
+        }else if (Character.isDigit(character)){
+            System.out.println("Entered character is a digit");
+        }else {
+            System.out.println("Enter a character or a digit or a special");
+        }
+    }
+}
