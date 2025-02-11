c85a98a0193c118060dfb92d818330bbb244ce82
vignan77020 <vignanreddynani@gmail.com>
2025-02-11 12:31:36 +0530

    to print 5 table

---
+++ Demo050.java
@@ -0,0 +1,9 @@
+package Core_Java;
+
+class Demo050{
+    public static void main(String[] args){
+        for(int i = 0;i<=10;i++){
+            System.out.println("5 * "+i+" = "+5*i);
+        }
+    }
+}
