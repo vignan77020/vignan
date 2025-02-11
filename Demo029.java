15eaf1043ba90c3143e1f7b86bcd606230994aec
vignan77020 <vignanreddynani@gmail.com>
2025-02-11 12:07:47 +0530

    Adding 10 names in arraylist

---
+++ Demo029.java
@@ -0,0 +1,29 @@
+package Core_Java;
+
+import java.util.*;
+
+class Demo029{
+    ArrayList<String> list = new ArrayList<String>();
+    void Add(String name){
+        list.add(name);
+    }
+    void input(){
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<10; i++){
+            System.out.print("Enter the "+(i+1)+" person name: ");
+            String name = sc.next();
+            Add(name);
+        }
+    }
+    void display(){
+        for(String name: list){
+            System.out.println(name);
+        }
+    }
+    public static void main(String[] args){
+        Demo029 obj = new Demo029();
+        obj.input();
+        System.out.println("\nNames are: ");
+        obj.display();
+    }
+}
