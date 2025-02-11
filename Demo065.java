283cb1701cd83f4512de3db992a6c3bfa4ef9ba0
vignan77020 <vignanreddynani@gmail.com>
2025-02-11 23:00:51 +0530

    prnting of array elements

---
+++ Demo065.java
@@ -0,0 +1,9 @@
+public class Demo065 {
+    public static void main(String[] args) {
+        int [] arr ={12,20,30,45,11};
+
+       for (int i : arr) {
+          System.out.println(i);
+       }
+    }
+}
